bab53400808e63d8deff672afade5025415d4aa8
Thanhduy170302 <thanhduy170302@gmail.com>
2023-07-21 02:18:01 +0700

    video27

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,13 +1,14 @@
 import React from "react";
+import './DisplayInfor.scss'
 class DisplayInfor extends React.Component {
 
     state = {
         isShowListUser: true
     }
     handleShowHide = () => {
-       this.setState({
-        isShowListUser:!this.state.isShowListUser
-       })
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
     }
     render() {
         //destructuring aray
@@ -17,13 +18,13 @@ class DisplayInfor extends React.Component {
         //props=> Viết tắt của từ properties
 
         return (
-            <div>
+            <div className="display-infor-container">
 
                 <div>
                     <span onClick={() => { this.handleShowHide() }}>
-                        
-                        {this.state.isShowListUser===true ? "Hide list user":"Show list user"}
-                        </span>
+
+                        {this.state.isShowListUser === true ? "Hide list user" : "Show list user"}
+                    </span>
                 </div>
                 {listUsers.map((user, index) => {
                     return (
@@ -31,7 +32,7 @@ class DisplayInfor extends React.Component {
                             {
                                 this.state.isShowListUser &&
                                 <div key={user.id} className={+user.age > 21 ? "green" : "red"}>
-                                    <div>My name's {user.name}</div>
+                                    <div >My name's {user.name}</div>
                                     <div>My age's  {user.age} </div>
                                     <hr />
                                 </div>
