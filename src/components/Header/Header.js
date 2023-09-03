3b0daa1727fc25aa70e74f391b0775f3925e9d26
Thanhduy170302 <thanhduy170302@gmail.com>
2023-09-04 01:25:45 +0700

    video51

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,6 +10,7 @@ const Header = () => {
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
                 <Navbar.Brand href="/">Thanh Duy Web</Navbar.Brand>
+                <NavLink to="/" className='navbar-brand'>Thanh Duy Web</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -22,9 +23,9 @@ const Header = () => {
 
                     </Nav>
                     <NavDropdown title="Settings" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+                        <NavDropdown.Item>Log in</NavDropdown.Item>
+                        <NavDropdown.Item>Log out</NavDropdown.Item>
+                        <NavDropdown.Item>Profile</NavDropdown.Item>
 
 
                     </NavDropdown>
