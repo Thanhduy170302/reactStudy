58267e8642f2c367a7f258ce77b92dfc1e786068
Thanhduy170302 <thanhduy170302@gmail.com>
2023-09-03 23:58:34 +0700

    video49

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Thanh Duy Web</Navbar.Brand>
+                <Navbar.Brand href="/">Thanh Duy Web</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
